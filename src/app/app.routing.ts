7e4feb45840fa1a8c3c82c5db8d44448d34971b9
NeilOberholzer <neiloberholzer2016@gmail.com>
2019-10-23 12:35:38 +0200

    Auto-generated commit

--- src/app/app.routing.ts
+++
@@ -1,6 +0,0 @@
-import { RouterModule } from '@angular/router';
-import { WeatherComponent } from './weather/weather.component';
-
-export const routing = RouterModule.forRoot([
-    {path: '', component: WeatherComponent}
-])
--- src/app/weather/format-weather.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'toFarenheit'})
-
-export class FormatWeather implements PipeTransform {
-    transform(C: number) {
-        return (C * 9 / 5 + 32).toFixed();
-    }
-}
